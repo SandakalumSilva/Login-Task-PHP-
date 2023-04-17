380904c25802f1b36667c247ef592316ea8bc173
SandakalumSilva <sandakalum1996@gmail.com>
2023-04-17 22:39:52 +0530

    validation function write 1

--- includes/register.inc.php
+++ includes/register.inc.php
@@ -1,11 +1,11 @@
-<?php 
+<?php
 
 require_once "./dbh.inc.php";
 
 // validation file 
 require_once "./validation.inc.php";
 
-if(isset($_POST["register-btn"])){
+if (isset($_POST["register-btn"])) {
 
     $fname = $_POST["fname"];
     $lname = $_POST["lname"];
@@ -14,26 +14,24 @@ if(isset($_POST["register-btn"])){
     $password = $_POST["password"];
     $re_password = $_POST["re_password"];
 
-if(inputsEmptyRegister($fname,$lname,$email,$mobile,$password,$re_password)){
-header("location: ../index.php?err=empty_inputs");
+    if (inputsEmptyRegister($fname, $lname, $email, $mobile, $password, $re_password)) {
+        header("location: ../index.php?err=empty_inputs");
+    } elseif (nameInvalid($fname, $lname)) {
+        header("location: ../index.php?err=invalid_name");
+    } elseif (emailInvalid($email)) {
+        header("location: ../index.php?err=invalid_email");
+    } elseif (mobileInvalid($mobile)) {
+        header("location: ../index.php?err=invalid_mobile");
+    } elseif (passwordInvalid($password)) {
+        header("location: ../index.php?err=invalid_password");
+    } elseif (notMatch($password, $re_password)) {
+        header("location: ../index.php?err=different_password");
+    } elseif (emailormobileavailable($conn, $mobile, $email)) {
+        header("location: ../index.php?err=available_emailormobile");
+    } else {
+        RegiasaterNewuser($conn, $fname, $lname, $email, $mobile, $password, $re_password);
+    }
+} else {
+    header("location: ../index.php");
+    exit();
 }
-elseif(nameInvalid($fname,$lname)){
-    header("location: ../index.php?err=invalid_name");
-}
-elseif(emailInvalid($email)){
-    header("location: ../index.php?err=invalid_email"); 
-}elseif(mobileInvalid($mobile)){
-    header("location: ../index.php?err=invalid_mobile");
-}elseif(passwordInvalid($password)){
-    header("location: ../index.php?err=invalid_password");
-}elseif(notMatch($password,$re_password)){
-    header("location: ../index.php?err=different_password");
-}elseif(emailormobileavailable($conn,$mobile,$email)){
-    header("location: ../index.php?err=available_emailormobile");
-}else{
-    RegiasaterNewuser($conn,$fname,$lname,$email,$mobile,$password,$re_password);
-}
-
-}
-
-?>
--- includes/validation.inc.php
+++ includes/validation.inc.php
@@ -1,3 +1,102 @@
-<?php 
+<?php
+// check if register inputs are empty 
 
-?>
+function inputsEmptyRegister($fname, $lname, $email, $mobile, $password, $re_password)
+{
+    $value;
+
+    if (empty($fname) || empty($lname) || empty($email) || empty($mobile) || empty($password) || empty($re_password)) {
+        $value = true;
+    } else {
+        $value = false;
+    }
+
+    return $value;
+}
+
+function nameInvalid($fname, $lname)
+{
+    $value;
+    if (!preg_match("/^[a-zA-Z]+$/", $fname)) {
+        $value = true;
+    } elseif (!preg_match("/^[a-zA-Z]+$/", $lname)) {
+        $value = true;
+    } else {
+        $value = false;
+    }
+
+    return $value;
+}
+
+
+function emailInvalid($email)
+{
+    $value;
+    if (!preg_match("/^[a-zA-Z\d\._-]+@[a-zA-Z\d\._-]+\.[a-zA-Z\d\._-]{2,}$/", $email)) {
+        $value = true;
+    } else {
+        $value = false;
+    }
+    return $value;
+}
+
+function mobileInvalid($mobile)
+{
+    $value;
+    if (!preg_match("/^[0][\d]{9}$/", $mobile)) {
+        $value = true;
+    } else {
+        $value = false;
+    }
+    return $value;
+}
+
+function passwordInvalid($password)
+{
+    $value;
+    if (!preg_match("/^.{5,}$/", $password)) {
+        $value = true;
+    } else {
+        $value = false;
+    }
+    return $value;
+}
+
+function notMatch($password, $re_password)
+{
+    $value;
+    if ($password !== $re_password) {
+        $value = true;
+    } else {
+        $value = false;
+    }
+    return $value;
+}
+
+function emailormobileavailable($conn, $mobile, $email)
+{
+    $value;
+
+    $sql = "SELECT*FROM users WHERE email = ? OR mobile = ?;";
+    $tsmt = mysqli_stmt_init($conn);
+
+    if (mysqli_stmt_prepare($tsmt, $sql)) {
+        header("location: ../index.php?err=failedstmt");
+        exit();
+    } else {
+        mysqli_stmt_bind_param($tsmt, "si", $email, $mobile);
+
+        mysqli_stmt_execute($tsmt);
+
+        $data = mysqli_stmt_get_result($tsmt);
+
+        if (mysqli_fetch_assoc($data)) {
+            $value = false;
+        } else {
+            $value = true;
+        }
+    }
+
+    mysqli_stmt_close($tsmt);
+    return $value;
+}
